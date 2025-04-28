9130e9948fed39d4e2493398cf8b8365912a84d8
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2025-04-28 18:52:03 -0400

    Add ridge and lasso regression, save evaluation text to file

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def main():
         os.mkdir("figures")
 
     for csv in dataset_list:
-        train_model(os.path.join(dataset_path, csv))
+        train_models(os.path.join(dataset_path, csv))
 
-def train_model(data_path):
+def train_models(data_path):
     data = pd.read_csv(data_path)
 
     # split the path to get the ticker name from the filename
@@ -55,28 +55,118 @@ def train_model(data_path):
 
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3, random_state=46)
 
-    linear = LinearRegression()
-    linear.fit(X_train, y_train)
-    y_pred = linear.predict(X_test)
+    def train_linear_regression():
+        linear = LinearRegression()
+        linear.fit(X_train, y_train)
+        y_pred = linear.predict(X_test)
 
-    # Evaluation
-    print('Accuracy: ', linear.score(X_test, y_test) * 100)
-    print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-    print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+        # Evaluation
+        evaluation_file = open(os.path.join(figure_save_path, "linear_regression_evaluation.txt"), "w")
 
-    # Visualization
-    a = X_test.open
-    b = y_test
-    c = X_test.open
-    d = y_pred
-    plt.figure(dpi=80)
-    plt.scatter(a, b)
-    plt.scatter(c, d)
-    plt.legend(["Test", "Predicted"])
-    plt.xlabel("open")
-    plt.ylabel("close")
-    plt.title(ticker_name)
-    plt.savefig(os.path.join(figure_save_path, "linear_regression.png"))
+        evaluation_file.write('Linear Regression Evaluation of ' + str(ticker_name) + '\n')
+        evaluation_file.write('Accuracy: ' + str(linear.score(X_test, y_test) * 100) + '\n')
+        evaluation_file.write('Mean Absolute Error: ' + str(metrics.mean_absolute_error(y_test, y_pred)) + '\n')
+        evaluation_file.write('Root Mean Squared Error: ' + str(np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+                              + '\n')
+
+        evaluation_file.close()
+
+        print('Linear Regression Evaluation of ', ticker_name)
+        print('Accuracy: ', linear.score(X_test, y_test) * 100)
+        print('Mean Absolute Error: ', metrics.mean_absolute_error(y_test, y_pred))
+        print('Root Mean Squared Error: ', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+        # Visualization
+        a = X_test.open
+        b = y_test
+        c = X_test.open
+        d = y_pred
+        plt.figure(dpi=80)
+        plt.scatter(a, b)
+        plt.scatter(c, d)
+        plt.legend(["Test", "Predicted"])
+        plt.xlabel("open")
+        plt.ylabel("close")
+        plt.title(ticker_name)
+        plt.savefig(os.path.join(figure_save_path, "linear_regression.png"))
+
+    def train_lasso():
+        lasso = Lasso(max_iter = 10000, alpha = 0.2)
+        lasso.fit(X_train, y_train)
+        y_pred = lasso.predict(X_test)
+
+        # Evaluation
+        evaluation_file = open(os.path.join(figure_save_path, "lasso_evaluation.txt"), "w")
+
+        evaluation_file.write('Lasso Evaluation of ' + str(ticker_name) + '\n')
+        evaluation_file.write('Accuracy: ' + str(lasso.score(X_test, y_test) * 100) + '\n')
+        evaluation_file.write('Mean Absolute Error: ' + str(metrics.mean_absolute_error(y_test, y_pred)) + '\n')
+        evaluation_file.write('Root Mean Squared Error: ' + str(np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+                              + '\n')
+
+        evaluation_file.close()
+
+        print('Lasso Evaluation of ', ticker_name)
+        print('Accuracy: ', lasso.score(X_test, y_test) * 100)
+        print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+        print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+
+        # Visualization
+        a = X_test.open
+        b = y_test
+        c = X_test.open
+        d = y_pred
+        plt.figure(dpi=80)
+        plt.scatter(a, b)
+        plt.scatter(c, d)
+        plt.legend(["Test", "Predicted"])
+        plt.xlabel("open")
+        plt.ylabel("close")
+        plt.title(ticker_name)
+        plt.savefig(os.path.join(figure_save_path, "lasso.png"))
+
+    def train_ridge():
+        ridge = Ridge(max_iter=10, alpha=0.1)
+        ridge.fit(X_train, y_train)
+        y_pred = ridge.predict(X_test)
+
+        # Evaluation
+        evaluation_file = open(os.path.join(figure_save_path, "ridge_evaluation.txt"), "w")
+
+        evaluation_file.write('Ridge Evaluation of ' + str(ticker_name) + '\n')
+        evaluation_file.write('Accuracy: ' + str(ridge.score(X_test, y_test) * 100) + '\n')
+        evaluation_file.write('Mean Absolute Error: ' + str(metrics.mean_absolute_error(y_test, y_pred)) + '\n')
+        evaluation_file.write('Root Mean Squared Error: ' + str(np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+                              + '\n')
+
+        evaluation_file.close()
+
+        print('Ridge Evaluation of ', ticker_name)
+        print('Accuracy: ', ridge.score(X_test, y_test) * 100)
+        print('Mean Absolute Error: ', metrics.mean_absolute_error(y_test, y_pred))
+        print('Root Mean Squared Error: ', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+        # Visualization
+        a = X_test.open
+        b = y_test
+        c = X_test.open
+        d = y_pred
+        plt.figure(dpi=80)
+        plt.scatter(a, b)
+        plt.scatter(c, d)
+        plt.legend(["Test", "Predicted"])
+        plt.xlabel("open")
+        plt.ylabel("close")
+        plt.title(ticker_name)
+        plt.savefig(os.path.join(figure_save_path, "ridge.png"))
+
+    train_linear_regression()
+    print()
+    train_lasso()
+    print()
+    train_ridge()
 
 if __name__ == '__main__':
     main()
